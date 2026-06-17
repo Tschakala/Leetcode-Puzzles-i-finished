e292d0b987c28a7cfaa84b12e0a030bb37c99dba
Raffael Brauner <raffael.brauner@outlook.com>
2026-06-17 15:18:59 +0200

    wdada

---
+++ Longest-Substring-Without-Repeating-Characters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "pwwkew";
+
+            Console.WriteLine(LengthOfLongestSubstring(s));
+        }
+
+        static int LengthOfLongestSubstring(string s)
+        {
+            if (s == string.Empty)
+            {
+                return 0;
+            }
+
+            int maxLength = 0;
+            List<char> current = new List<char>();
+
+            foreach (char c in s)
+            {
+                while (current.Contains(c))
+                {
+                    current.RemoveAt(0);
+                }
+
+                current.Add(c);
+                maxLength = Math.Max(maxLength, current.Count);
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ Sudoku-Solver/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Solver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void SolveSudoku(char[][] board)
+        {
+            List<char> list = new List<char>();
+            int num;
+            bool contain;
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board.Length; j++)
+                {
+                    if (board[i][j] == '.')
+                    {
+                        num = 1;
+                        contain = false;
+
+                        while (!contain)
+                        {
+                            list.Clear();
+                            list.AddRange(board[i]);
+                            if (list.Contains(char.Parse(num.ToString())))
+                            {
+                                contain = true;
+                                num++;
+                            }
+                            else
+                            {
+                                board[i][j] = char.Parse(num.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+}
