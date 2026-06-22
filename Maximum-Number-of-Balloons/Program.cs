6ea5c417556c6ff0b1ad207e326f4a57dc4e3044
Raffael Brauner <raffael.brauner@outlook.com>
2026-06-22 21:01:22 +0200

    Maximum-Number-of-Balloons

---
+++ Maximum-Number-of-Balloons/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Number_of_Balloons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "nlaebolko";
+            Console.WriteLine(MaxNumberOfBalloons(text));
+        }
+
+        static int MaxNumberOfBalloons(string text)
+        {
+            List<char> list = new List<char>();
+            list.AddRange(text);
+            string balloon = "balloon";
+            int c = 0;
+            int count = list.Count();
+            int x = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (x >= 7)
+                {
+                    x = 0;
+                }
+                if (list.Contains(balloon[x]))
+                {
+                    c++;
+                    list.Remove(balloon[x]);
+                    x++;
+                }
+                else
+                {
+                    return c / 7;
+                }
+            }
+
+            return c / 7;
+        }
+    }
+}
---
+++ Maximum-Number-of-Balloons/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Maximum-Number-of-Balloons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Maximum-Number-of-Balloons")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("89723ee4-1c5d-48e8-8e19-8bc99ce1cf8e")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
